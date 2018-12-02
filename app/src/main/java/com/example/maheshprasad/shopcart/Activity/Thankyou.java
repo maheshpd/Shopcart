50714bc74b1984c15bb0d52bda8fb9f04afa899e
maheshprasad <mahesh223prasad@gmail.com>
2018-12-02 12:16:57 +0530

    Complite Thank you layout and payment option layout

---
+++ app/src/main/java/com/example/maheshprasad/shopcart/Activity/Thankyou.java
@@ -0,0 +1,15 @@
+package com.example.maheshprasad.shopcart.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.maheshprasad.shopcart.R;
+
+public class Thankyou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thankyou);
+    }
+}
